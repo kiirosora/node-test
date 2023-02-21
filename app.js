c17a56fdf08489223e5550e0ff2747c83aa35ad4
kiirosora <kiirosora.code@gmail.com>
2023-02-21 17:01:56 +0800

    Added: socket.io

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  res.send("Hello World!");
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log('listening on *:' + port);
 });
